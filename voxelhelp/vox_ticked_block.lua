5d4430568e42ff063efecc2239cb87d2e2a9e1c9
Voxel Help API <157220922+VoxHelp@users.noreply.github.com>
2024-02-04 12:01:31 +0900

    vox_ticked_block.tick fix

--- voxelhelp/vox_ticked_block.lua
+++ voxelhelp/vox_ticked_block.lua
@@ -7,19 +7,23 @@ local vox_ticked_block = { onTick = nil }
 local tickedBlocksTable = nil
 local subscribedBlocks = nil
 local blockName = nil
+local blockId = nil
 local initialized = false
 
 function vox_ticked_block:tick(tps)
 	if subscribedBlocks:size() > 0 then
 	    for i = 0, subscribedBlocks:iterations() do
-	    	local block = subscribedBlocks:get(i)
+	    	local subBlock = subscribedBlocks:get(i)
+	    	local subBlockId = get_block(subBlock.x, subBlock.y, subBlock.z)
 
-	    	if block_name(get_block(block.x, block.y, block.z)) ~= blockName then
-		    	subscribedBlocks:removeAt(i)
-		    	i = i - 1
-		    else
-		    	vox_ticked_block.onTick(block.x, block.y, block.z, tps)
-		    end
+	    	if subBlockId ~= -1 then
+		    	if subBlockId ~= blockId then
+			    	subscribedBlocks:removeAt(i)
+			    	i = i - 1
+			    else
+			    	vox_ticked_block.onTick(subBlock.x, subBlock.y, subBlock.z, tps)
+			    end
+			end
 	    end
 	end
 end
@@ -58,7 +62,12 @@ function vox_ticked_block:initialized()
 end
 
 function vox_ticked_block:initialize(_blockName)
+	if vox_ticked_block:initialized() then
+		error("Voxel Help Ticked Block: Already initialized")
+	end
+
 	blockName = _blockName
+	blockId = block_id(blockName)
 
 	subscribedBlocks = vox_arrays.toList(vox_tables.alwaysTable(vox_metadata.getGlobalMetaByPath(blockName, { "voxelhelp", "tickedBlocks" })))
 
