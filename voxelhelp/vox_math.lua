8890edbdb1dfdc975abb935afcb1ec78f7c23c26
Cogitary <84321459+DoubleDataStack@users.noreply.github.com>
2024-02-17 13:28:34 +0300

    Update vox_math.lua

--- voxelhelp/vox_math.lua
+++ voxelhelp/vox_math.lua
@@ -22,7 +22,7 @@ function vox_math.e()
 end
 
 function vox_math.pi()
-    return 3.14159265358979323846
+    return 3.14159265358979
 end
 
 function vox_math.positive_infinity()
@@ -43,22 +43,25 @@ function vox_math.ceil(n)
     return n > nint and nint + 1 or nint
 end
 
--- Part by function (kraject)
+-- Part by Cogitary
 
 function vox_math.factorial(n)
     if n == 0 then return 1
-    elseif n<0 then error("err [n!]<0")
+    elseif n<0 then error("Voxel Help Math: Parameter \"n\" must always be greater than -1")
     else return n * vox_math.factorial(n - 1) end
+end
+
+function vox_math.factorialSaves(n) -- non stack overflow
+    if n < 0 then error("Voxel Help Math: Parameter \"n\" must always be greater than -1") end
+    local f = 1 for i = 2, n do f = f * i end
+    return f
 end
 
 function vox_math.pow(base, power)
     return base ^ power
 end
-
-function vox_math.clamp(n,min,max)
-    if n > max then return max
-    elseif n < min then return min
-    else return n end
+function vox_math.clamp(n,min,max) 
+    return n > max and max or n < min and min or n 
 end
 
 function vox_math.floor(n)
@@ -78,37 +81,56 @@ function vox_math.lcm(a, b) -- нок
     return a*b / vox_math.gcd(a, b)
 end
 
-function vox_math.reduct(a, b) --сокращает дроби
-    -- 5/10 -> 1/2
-    -- 3/90 -> 1/30
-    -- 2/54 -> 1/27 
+
+--  Cокращает дроби
+--- Приводит дробь к ее простейшей форме.
+-- @param a — числитель дроби.
+-- @param b — знаменатель дроби.
+-- @return два целых числа, представляющих сокращенную форму дроби.
+-- Пример: 5/10 уменьшается до 1/2, 
+--         3/90 уменьшается до 1/30, 
+--         2/54 уменьшается до 1/27.
+-- использование: vox_math.reduct(5,10)
+function vox_math.reduct(a, b) 
    local k = vox_math.gcd(a,b)
    return vox_math.int(a/k), vox_math.int(b/k)
 end
 
-function vox_math.sign(n) -- определения знака
-    if n == 0 then
-        return 0
-    end
 
-    return n / vox_math.abs(n)
+function vox_math.sign(n)-- определения знака
+    return n == 0 and 0 or n / vox_math.abs(n)
 end
-
-function vox_math.modf(n) -- возврат целой и дробной части
+-- Возврат целой и дробной части
+-- @param  число
+-- @return целая часть, дробная часть числа
+function vox_math.modf(n) 
     return vox_math.floor(n),vox_math.abs(vox_math.floor(n)-n)
 end
 
-local function F(x)
-    if x~=0 then return 1/x end  
-end
 
-function vox_math.integral(f,a,b) -- определённый интеграл 
-    -- f <=> f(x) 
-    -- например интегрирование f(x)= sin(x) от 0 до 1.57..., тогда
-    --  integral(sin,0,1.57...) => 0.999... --> 1.
-    --  integral(sin,-pi,pi) => 0
-    --  [function vox_math.F(...)], a,b=... => integral(vox_math.F,a,b)
-    --  [function G(...)], a1,a2=... => integral(G, a1, a2)
+
+--- Вычисляет определенный интеграл функции `f` на интервале `[a, b]`.
+-- Функция использует правило трапеций для аппроксимации определенного интеграла.
+-- @function vox_math.integral
+-- @param f Функция для интегрирования.
+-- @param a Начало интервала интегрирования.
+-- @param b Конец интервала, по которому производится интегрирование.
+-- @return Примерное значение интеграла.
+
+-- @usage
+-- -- Пример: аппроксимировать интеграл sin(x) от 0 до pi/2 (1,57...)
+-- print(vox_math.integral(math.sin, 0, 1.57)) -- Выводит 1
+--
+-- -- Пример: аппроксимировать интеграл sin(x) от -pi до pi
+-- print(vox_math.integral(math.sin, -math.pi, math.pi)) -- Выводит 0
+
+-- Пример: вычисление натурального логарифма с использованием определенного интеграла
+--    function vox_math.ln(n)
+--        if n > 0 then 
+--             return vox_math.integral(F,1,n) 
+--        end
+--    end
+function vox_math.integral(f,a,b) 
     local steps = 1e6
     local h = (b - a) / steps
     local sum = 0.5 * (f(a) + f(b))
@@ -119,59 +141,42 @@ function vox_math.integral(f,a,b) -- определённый интеграл
 end
 
 
-function vox_math.D(f,x) --диффур
-    --      dy/dx <=> D(y, x) -- df/dx == D(f,x)
-    --      [function vox_math.sin(t)] => D(vox_math.sin, x) 
-    --      x=0 => D(sin, x) == 1  [y=sin(x) => dy/dx=cos(x)]
-    --      omega=12, [function F(...)] => D(F, omega) == ...
-
-    local h = 1e-12
-    return (f(x+h)-f(x))/h
-end
-
-function vox_math.log(n, base) -- лог-м
-    if base > 0 or base ~= 1 or n > 0 then
-        return vox_math.integral(F,1,n) / vox_math.integral(F,1,base)
-    end
-end
-
-function vox_math.ln(n) -- натуральный лог-м
-    if n > 0 then return vox_math.integral(F,1,n) end
-end
-
-function vox_math.log10(n) -- десятичный лог-м ... log10 == lg == log(n=..., base=10)
-    if n > 0 then return vox_math.integral(F,1,n)/vox_math.ln(10) end
+-- Диффур
+--- Функция работает как простой численный дифференциал (дифференциальный оператор)
+-- Например, если у нас есть функция f(x) = sin(x) в точке x = 0 и мы хотим взять производную
+--  f'(x) = cos(x), тогда мы будем использовать эту функцию как D(sin, 0) и результат должен быть 1, так как cos(0) = 1.
+-- @param f - функция для дифференциации
+-- @param x - точка, в которой будет рассчитана производная
+-- @return производная функции в точке x
+function vox_math.D(f, x)
+    local h = 1e-12  -- точность 
+    return (f(x + h) - f(x)) / h
 end
 
 
-function vox_math.exp(x) -- экспанента
+--- Экспанента
+-- @param x - аргумент экспоненты
+-- @return значение экспоненты в точке x
+function vox_math.exp(x)
     return vox_math.e()^x
 end
 
-function vox_math.sqrt(n) --радикал
-    if n >= 0 then return n^(1/2) end
-end
-
-function vox_math.inverse_sqrt(x)
-    return 1/vox_math.sqrt(x)
-end
 
 function vox_math.modf(n) -- возврат целой и дробной части
     return vox_math.floor(n),vox_math.abs(vox_math.floor(n)-n)
 end
 
+
 function vox_math.dist(x0,y0,z0,x1,y1,z1) -- дистанция от A(x0,y0,z0) до B(x1,y1,z1)
-    if x0 and y0 and z0 and x1 and y1 and z1 ~= nil then 
-        return ((x1-x0)^2+(y1-y0)^2+(z1-z0)^2)^(1/2)
-    end
+    return ((x1-x0)^2+(y1-y0)^2+(z1-z0)^2)^(1/2)
 end
 
 function vox_math.dist2(x0,y0,x1,y1) 
-    return vox_math.sqrt((x1-x0)^2+(y1-y0)^2)
+    return ((x1-x0)^2+(y1-y0)^2)^0.5
 end
 
 function vox_math.hypot(x,y)
-    return vox_math.sqrt(x*x+y*y)
+    return (x*x+y*y)^0.5
 end
 
 function vox_math.distline(x0,x1)
@@ -179,57 +184,8 @@ function vox_math.distline(x0,x1)
 end
 
 
---trigonometric func
-function vox_math.sin(x)
-    local t = x%(vox_math.pi()*2)
-    local term
-    local sin = 0
-    local sign = 1
-    local pow = 1
-    local epsilon = 1e-16
-
-    while true do
-        term = sign * ((t ^ pow) /  vox_math.factorial(pow))
-        sin = sin + term
-        if vox_math.abs(term) < epsilon then
-            break
-        end
-        sign = -sign
-        pow = pow + 2
-    end
-    return sin
-
-end
-
-function vox_math.cos(x)
-    return vox_math.sin(x + vox_math.pi()/2)
-end
-
-
-function vox_math.tan(n)  -- tg == tan
-    if n~=0 then return vox_math.sin(n)/vox_math.cos(n) end
-end
-
-function vox_math.ctan(n) -- ctg == cot == ctan
-    if n~=0 then return vox_math.cos(n)/vox_math.sin(n) end
-end
-
-function vox_math.sec(n)
-    if n~=0 then return 1/vox_math.cos(n) end
-end
-
-function vox_math.cosec(n)
-    if n~=0 then return 1/vox_math.sin(n) end
-end
-
--- reverse trigonometric func
-function vox_math.atan2(y, x)
-    local func = 0
-    local k = y/x
-    for n=0,1000 do
-        func = func + ((-1)^n)*((k^(2*n+1))/(2*n+1))
-    end
-    return func
+function vox_math.minmax(num1,num2)
+    return vox_math.min(num1,num2), vox_math.max(num1,num2)
 end
 
 
@@ -237,92 +193,109 @@ end
 -- hyperbolic func
 -- x = ch(t)
 -- y = sh(t)
-function vox_math.sh(n) -- гипербо-ий синус
+function math.sinh(n) -- гипербо-ий синус
     return ((vox_math.e()^n)-(vox_math.e()^(-n)))/2
 end
 
-function vox_math.ch(n) -- гипербо-ий косинус
+function math.cosh(n) -- гипербо-ий косинус
     return ((vox_math.e()^n)+(vox_math.e()^(-n)))/2
 end
 
-function vox_math.tanh(x)
+function vox_math.tanh(x) -- гипербо-ий тангенс
     return vox_math.sh(x)/vox_math.ch(x)
 end
 
-function vox_math.cth(x)
+function vox_math.coth(x) -- гипербо-ий котангенс
     return 1/vox_math.tanh(x)
 end
 
-function vox_math.sch(x)
+function vox_math.sch(x) -- гипербо-ий секас
     if x ~= 0 then return 1/vox_math.ch(x) end
 end
 
-function vox_math.csch(x)
+function vox_math.csch(x) -- гипербо-ий косекас
     if x ~= 0 then return 1/vox_math.sh(x) end
 end
 
-function vox_math.arsh(x)
-    return vox_math.ln(x+vox_math.sqrt(x^2+1))
+function vox_math.arsh(x) -- гипербо-ий обратный синус
+    return vox_math.ln(x+(x^2+1)^0.5)
 end
 
-function vox_math.arth(x)
+function vox_math.arth(x) -- гипербо-ий обратный тангенс
     if vox_math.abs(x)<1 then 
-        return vox_math.ln((vox_math.sqrt(1-x^2))/(1-x))
+        return vox_math.ln(((1-x^2)^0.5)/(1-x))
     end
 end
 
-function vox_math.arch(x)
-    if x >= 1 then return vox_math.ln(x+vox_math.sqrt(x^2-1)) end
+function vox_math.arch(x) -- гипербо-ий обратный косинус
+    if x >= 1 then return vox_math.ln(x+(x^2-1)^0.5) end
 end
 
-function vox_math.arcth(x)
+function vox_math.arcth(x) -- гипербо-ий обратный котангенс
     if vox_math.abs(x) > 1 then 
-        return vox_math.ln((vox_math.sqrt(x^2-1))/(x-1))
+        return vox_math.ln(((x^2-1)^0.5)/(x-1))
     end
 end
 
-function vox_math.arsch(x)
+function vox_math.arsch(x) -- гипербо-ий обратный секас
     if x > 0 and x <= 1 then
-        return vox_math.ln((1+vox_math.sqrt(1-x^2))/(x))
+        return vox_math.ln((1+(1-x^2)^0.5)/(x))
     end
 end
 
 ------------------------------------------------
 
+-- Операции перевода
 
 
--- oper translate
-function vox_math.rad(alpha) -- из градусов в радианы
-    return (alpha*vox_math.pi())/180
-end
-
-function vox_math.deg(alpha) --из радианов в градусы
-    return (alpha*180)/vox_math.pi()
-end
-
-function vox_math.toPolar(x,y) -- переход на полярные координаты
+-- Переход к полярным координатам
+-- @param x  x-координата в декартовой системе координат
+-- @param y  y-координата в декартовой системе координат
+-- @return r  Радиус в полярной системе координат
+-- @return phi  Угол в полярной системе координат
+function vox_math.toPolar(x,y)
     local r = (x^2+y^2)^0.5
-    local phi = vox_math.atan2(y,x)
+    local phi = math.atan2(y,x)
     return r, phi
 end
 
-function vox_math.toCart(x,y) -- переход на декартовые координаты
+-- Переход к декартовым координатам
+-- @param x  Радиус в полярной системе координат
+-- @param y  Угол в полярной системе координат
+-- @return  x-координата в декартовой системе координат
+-- @return  y-координата в декартовой системе координат
+function vox_math.toCart(x,y)
     local r = x
     local phi = y
-    return (r * vox_math.cos(phi)), (r * vox_math.sin(phi))
+    return (r * math.cos(phi)), (r * math.sin(phi))
 end
 
-function  vox_math.toCylind(x, y, z) -- переход в цилиндрические координаты
-    local r = vox_math.sqrt(x*x + z*z)
-    local theta = vox_math.atan2(z, x) --возврат угла в градусах
+-- Переход к цилиндрическим координатам
+-- @param x  x-координата в декартовой системе координат
+-- @param y  y-координата в декартовой системе координат
+-- @param z  z-координата в декартовой системе координат
+-- @return r  Радиус в цилиндрической системе координат
+-- @return theta  Угол в цилиндрической системе координат
+-- @return y  y-координата в декартовой системе координат (неизменная)
+function  vox_math.toCylind(x, y, z)
+    local r = (x*x + z*z)^0.5
+    local theta = math.atan2(z, x)
     return r, theta, y
 end
 
---Static
-function vox_math.Binomial(n, k)
+
+-- Static
+
+-- Расчёт биномиального коэффициента nk
+-- (Выполняет расчёт количества возможностей выбора k элементов из множества длиною в n элементов без учёта порядка)
+-- @param n Количество элементов в множестве
+-- @param k Количество выбранных элементов
+-- @return Число комбинаций
+function vox_math.binomial(n, k)
     if (k < 0) or (n < 0) or (k > n) then
-        error("404")
+        error("Voxel Help Math: Неверный(ые) параметр(ы)")
     end
+
     if k > n/2 then
         k = n - k
     end
@@ -334,47 +307,28 @@ function vox_math.Binomial(n, k)
     return result
 end
 
+
+-- Расчёт количества комбинаций из n элементов по k
+-- (Выполняет расчёт количества способов выбора k элементов из множества n элементов без учёта порядка)
+-- @param n Количество элементов в множестве
+-- @param k Количество выбранных элементов
+-- @return Число комбинаций
+function vox_math.combinations(n, k) 
+    return vox_math.factorial(n) / (vox_math.factorial(k) * vox_math.factorial(n - k))
+end
+
+-- Подсчёт количества способов выбора n элементов из множества m
+-- (Выполняет расчёт количества перестановок из n элементов без учёта порядка)
+-- @param n Количество выбранных элементов
+-- @param m Количество элементов в множестве
+-- @return Число комбинаций или nil и сообщение об ошибке
 function vox_math.A(n, m)
-    if n < 0 or m < 0 or n > m then
-        return nil, "Invalid input"
+    if n < 0 or m < 0 or n > m then 
+        return nil, "Некорректный ввод" 
     end
     local result = 1
-    for i = m, m-n+1, -1 do
-        result = result * i
+    for i = m, m-n+1, -1 do 
+        result = result * i 
     end
     return result
 end
-
---Experimental Func (Random)
-function vox_math.generateRandomSeed(iteration) -- 
-    local time = world.get_day_time() / iteration * vox_math.randomLargeInt()
-    local  worldSeed = world.get_seed();
-    return time * (worldSeed * 2)
-end
-
-
-function vox_math.randomLargeInt() -- Большие числа (Не зависит от итераций)
-    return vox_math.round(vox_math.random()*vox_math.pi()*78319*world.get_day_time(),0)
-end
-
-function vox_math.randomInt(min, max, iteration) -- Числа на отрезке (Зависит от итераций)
-    if iteration == nil then
-        iteration = world.get_day_time() / 59
-    end
-    
-    local m = 2385482343
-    local aa = 48271      
-    local c = 0          
-    local seed = vox_math.generateRandomSeed(iteration)       
-
-    seed = (min * seed + c) % m
-
-    return vox_math.abs(vox_math.floor((seed / (m / (max - min + 1))) + min))
-end
-
-function vox_math.random()
-    local daytime = world.get_day_time()
-    return (vox_math.abs(vox_math.sin(3/(daytime-0.5))/vox_math.cos(2/(daytime-0.5))))
-end
-
-return vox_math
